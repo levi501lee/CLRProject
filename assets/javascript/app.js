dfa1c443abf6f9af9ce621ed54c40c8df2cff536
colinSeven8 <ccnorse80@gmail.com>
2019-10-05 11:48:09 -0600

    Trying to commit so I can GET the currente changes from the master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,32 +16,70 @@ $('#btn-start').mouseover(function () {
 
 });
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD-2KTAIkrBfmuFq30tQ0OnpbrMRbCebYs",
+  authDomain: "taste-in-movies.firebaseapp.com",
+  databaseURL: "https://taste-in-movies.firebaseio.com",
+  projectId: "taste-in-movies",
+  storageBucket: "",
+  messagingSenderId: "540309488028",
+  appId: "1:540309488028:web:3b41501682e6d24089c859"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Initialize database
+let database = firebase.database();
+
+//Get a reference from the first modal form
+const getLaidForm = document.querySelector('#getLaidForm');
+//Get a reference from the second modal form
+const worstDateForm = document.querySelector('#worstDateForm');
+
+$('#getLaid').on('click', function () {
+
+  let street = $(this).querySelector('#street').val().trim();
+  let city = $(this).querySelector('#city').val().trim();
+  let state = $(this).querySelector('#state').val().trim();
+  let zip = $(this).querySelector('#zip').val().trim();
+  let movieType = $(this).querySelector('#movieType').val().trim();
+});
+
+$('#worstDate').on('click', function () {
+
+  let street = $(this).querySelector('#street').val().trim();
+  let city = $(this).querySelector('#city').val().trim();
+  let state = $(this).querySelector('#state').val().trim();
+  let zip = $(this).querySelector('#zip').val().trim();
+  let movieType = $(this).querySelector('#movieType').val().trim();
+});
 
 
 /*RJ js for the modal*/
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("getLaidForm");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("getLaid");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close1")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+btn1.onclick = function () {
+  modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
   }
 }
 
